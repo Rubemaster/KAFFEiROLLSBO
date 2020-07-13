fb9220ba51919defc2d1d29bc3da636eae287dfe
Rubemaster <ruben.rick@outlook.com>
2020-07-13 16:31:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Hello who?</title>
+    <title>Hello who who?</title>
     <!-- Denali CSS & Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/denali-icon-font/dist/denali-icon-font.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/denali-css/css/denali.css">
