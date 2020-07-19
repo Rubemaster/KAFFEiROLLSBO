854bd8385e57fefd7239535c3f6f4435e639d0b8
Rubemaster <ruben.rick@outlook.com>
2020-07-19 20:56:14 +0200

    Update search.php

--- DB_ACCESS/search.php
+++ DB_ACCESS/search.php
@@ -4,13 +4,12 @@ if(!isset($_SESSION["SERVERNAME"])){$_SESSION["SERVERNAME"] = $_POST["SERVERNAME
 if(!isset($_SESSION["USERNAME"])){$_SESSION["USERNAME"] = $_POST["USERNAME"];}
 if(!isset($_SESSION["PASSWORD"])){$_SESSION["PASSWORD"] = $_POST["PASSWORD"];}
 if(!isset($_SESSION["DBNAME"])){$_SESSION["DBNAME"] = $_POST["DBNAME"];}
-if(!isset($_SESSION["SEARCH_TERM"])){$_SESSION["SEARCH_TERM"]=$_POST["SEARCH_TERM"];}
 
 $servername = $_SESSION["SERVERNAME"];
 $username = $_SESSION["USERNAME"];
 $password = $_SESSION["PASSWORD"];
 $dbname = $_SESSION["DBNAME"];
-$search_term=$_SESSION["SEARCH_TERM"];
+$search_term=$_POST["SEARCH_TERM"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -20,15 +19,8 @@ if ($conn->connect_error) {
   echo "Connection Seuccessful";
   $sql = "SELECT REFERENCE, PROJECT_ID, PAYEE_ID, COMPANY_ID, ITEMS_FROM, ITEMS_TO FROM INVOICES WHERE ID='$search_term'";
   $result = $conn->query($sql);
-  $results =[];
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      array_push($results,$row["REFERENCE"]);
-      echo "REFERENCE: " . $row["REFERENCE"]. "<br>";
-    }
-  }
   
-  $_SESSION['RESULTS']=$results;
+  $_SESSION['RESULT']=$result;
   header("Location: ./../records/search/");
   $conn->close();
   
