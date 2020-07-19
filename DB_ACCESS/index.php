5048828c7e7a76baf820e9671cb88f63f3c3a9fc
Rubemaster <ruben.rick@outlook.com>
2020-07-19 11:45:43 +0200

    Update index.php

--- DB_ACCESS/index.php
+++ DB_ACCESS/index.php
@@ -1,15 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$servername = $_POST["SERVERNAME"];
+$username = $_POST["USERNAME"];
+$password = $_POST["PASSWORD"];
+$dbname = $_POST["DBNAME"];
+$talebname = $_POST["TABLEBNAME"];
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+}else{
+  echo "Connection Seuccessful";
 }
+if($tablename=="INVOICES"){
+  $invoice_reference  = $_POST["INVOICE_REFERENCE"];
+  $invoice_project_id = $_POST["INVOICE_PROJECT_ID"];
+  $invoice_payee_id   = $_POST["INVOICE_PAYEE_ID"];
+  $invoice_company_id = $_POST["INVOICE_COMPANY_ID"];
+  $invoice_items_from = $_POST["INVOICE_ITEMS_FROM"];
+  $invoice_items_to   = $_POST["INVOICE_ITEMS_TO"];
+  $invoice_binary     = $_POST["INVOICE_BINARY"];
+}
+
+// Create connection
+// Check connection
 
 $sql = "INSERT INTO MyGuests (firstname, lastname, email)
 VALUES ('John', 'Doe', 'john@example.com')";
