2c076a037e72f4121176dcfad45f6a9f54792741
Rubemaster <ruben.rick@outlook.com>
2020-07-19 20:23:18 +0200

    Update search.php

--- DB_ACCESS/search.php
+++ DB_ACCESS/search.php
@@ -7,7 +7,7 @@ $search_term=$_POST["SEARCH_TERM"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
+header("Location: https://rubenrick.online/records/search/");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }else{
@@ -25,6 +25,6 @@ if ($conn->connect_error) {
   session_register('RESULTS');
   $_SESSION['RESULTS']=$results;
   $conn->close();
-  header("Location: https://rubenrick.online/records/search/");
+  
 }
 ?>
