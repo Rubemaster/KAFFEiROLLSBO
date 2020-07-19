acf53303cd75065c1eec371e37ceb2a7882c431d
Rubemaster <ruben.rick@outlook.com>
2020-07-19 19:35:21 +0200

    Update push-record.php

--- DB_ACCESS/push-record.php
+++ DB_ACCESS/push-record.php
@@ -47,7 +47,7 @@ if($tablename=="INVOICES"){
   echo " to ";
   echo $invoice_items_to;
   $sql = "INSERT INTO INVOICES (REFERENCE, PROJECT_ID, PAYEE_ID, COMPANY_ID, ITEMS_FROM, ITEMS_TO, PDF_BINARY) 
-          VALUES ('$invoice_reference', '$invoice_payee_id', '$invoice_project_id','$invoice_payee_id','$invoice_company_id','$invoice_items_from','$invoice_items_to','$invoice_binary')";
+          VALUES ('$invoice_reference', '$invoice_project_id', '$invoice_payee_id','$invoice_company_id','$invoice_items_from','$invoice_items_to','$invoice_binary')";
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
   } else {
