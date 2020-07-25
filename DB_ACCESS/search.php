d8a6d31b12288f8592f873d0645bb9aab1b3e5c6
Rubemaster <ruben.rick@outlook.com>
2020-07-25 13:00:37 +0200

    Update search.php

--- DB_ACCESS/search.php
+++ DB_ACCESS/search.php
@@ -3,7 +3,18 @@
   <body>
     <p>Thinking</p>
     <form id="PUSH_RESULT_BACK" action="./../records/search" method="post">
-      
+      <?php
+        $conn = new mysqli($_POST["SERVERNAME"], $_POST["USERNAME"], $_POST["PASSWORD"], $_POST["DBNAME"]);
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }else{
+          $search_term=$_POST["SEARCH_TERM"];
+          $sql = "SELECT REFERENCE, PROJECT_ID, PAYEE_ID, COMPANY_ID, ITEMS_FROM, ITEMS_TO FROM INVOICES WHERE ID='$search_term'";
+          $result = $conn->query($sql);
+          echo "<input type=\"hidden\" name=\"RESULT\" value='$result'>";
+        }
+        $conn->close();
+      ?>
     </form>
     <script>
       document.getElementById("PUSH_RESULT_BACK").submit();
