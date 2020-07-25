4b75477107d0fa151713f9cf27a50a25f4c604c9
Rubemaster <ruben.rick@outlook.com>
2020-07-25 11:48:47 +0200

    Update search.php

--- DB_ACCESS/search.php
+++ DB_ACCESS/search.php
@@ -26,7 +26,7 @@ if ($conn->connect_error) {
   echo "<i>Username:</i> ".$_SESSION["USERNAME"]."<br>";
   echo "<i>Password:</i> ".$_SESSION["PASSWORD"]."<br>";
   echo "<i>Database:</i> ".$_SESSION["DBNAME"]."<br>";
-  echo "<b><i>Connection Seuccessful</i><b/><br>";
+  echo "<b><i>Connection Seuccessful</i></b><br>";
   echo "<i>Result:</i> ";
   echo $_SESSION["RESULT"]."<br>";
   
