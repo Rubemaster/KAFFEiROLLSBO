c3eb5857e6446cacc060f644daebca5c14651d5b
Rubemaster <ruben.rick@outlook.com>
2020-07-25 14:13:37 +0200

    Update search.php

--- DB_ACCESS/search.php
+++ DB_ACCESS/search.php
@@ -11,8 +11,8 @@
           echo "Connection successful";
           $search_term=$_POST["SEARCH_TERM"];
           $search_by=$_POST["SEARCH_BY"];
-          $tablename=$_POST["TABLENAME"];
-          $sql = "SELECT REFERENCE, PROJECT_ID, PAYEE_ID, COMPANY_ID, ITEMS_FROM, ITEMS_TO FROM '$tablename' WHERE '$search_by'='$search_term'";
+          $tablename=$_POST["TABLE_NAME"];
+          $sql = "SELECT REFERENCE, PROJECT_ID, PAYEE_ID, COMPANY_ID, ITEMS_FROM, ITEMS_TO FROM INVOICES WHERE '$search_by'='$search_term'";
           $result = $conn->query($sql);
           $current=0;
           echo '<input type="hidden" name="RESULT_COUNT" value="'.$result->num_rows.'">';
