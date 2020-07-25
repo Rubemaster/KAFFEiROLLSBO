f259f1feb352e488d041d9f9791633676e89faaa
Rubemaster <ruben.rick@outlook.com>
2020-07-25 12:54:43 +0200

    Update index.php

--- records/search/index.php
+++ records/search/index.php
@@ -85,8 +85,8 @@
         $preresults="<div class=\"container\"><div class=\"row p-10 br-4\"><div class=\"col p-10 br-4\" style=\"background-color: #002B66\"><div class=\"row p-10 br-4\"><div class=\"col p-10 br-4\">";
         $postresults="</div></div></div></div></div>";
         echo $preresults;
-        if(isset($_SESSION["RESULT"])){
-          $result=$_SESSION["RESULT"];
+        if(isset($_POST["RESULT"])){
+          $result=$_POST["RESULT"];
           echo "<p style=\"color: white\"><b><i>RESULT COUNT:</b>".$result->num_rows."</i></p>";
           if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
